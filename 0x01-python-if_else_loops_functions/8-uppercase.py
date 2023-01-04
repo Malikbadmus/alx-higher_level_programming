efa79ec500da7afe1c8b760f1b143f5638d604eb
malik badmus <malik.badmus@icloud.com>
2023-01-04 13:41:03 -0800

    correction

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     x = ""
-    z = ""
     for i in str:
         if(ord(i) >= 97 and ord(i) <= 122):
             x = ord(i) - 32
             i = chr(x)
-            z = z + i
-    print("{}".format(z), end="")
+            print("{}".format(i), end="")
