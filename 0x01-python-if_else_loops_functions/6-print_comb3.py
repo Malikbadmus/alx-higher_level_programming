e7927e3c7aa4cf92e33c973dfaf878e31952ed45
malik badmus <malik.badmus@icloud.com>
2023-01-03 21:31:54 -0800

    correction

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-for i in range(1, 5):
-    for x in range(2, 9):
+for i in range(0, 9):
+    for x in range(i+1, 10):
+        if i == x:
+            continue
         print("{}{}".format(i, x), end = ", ")
-print("{:d}".format(76))
+print("\b\b\n",end="")
