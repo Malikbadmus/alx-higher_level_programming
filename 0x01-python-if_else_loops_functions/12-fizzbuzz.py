fede867d2763ba8543168fbc46d480d56af5ebfa
malik badmus <malik.badmus@icloud.com>
2023-01-04 14:20:11 -0800

    correction

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,8 +4,8 @@ def fizzbuzz():
     for i in range(1, 101):
         if (i % 3 == 0):
             i = Fizz
-        elif (i % 5 === 0):
+        elif (i % 5 == 0):
             i = Buzz
-        elif (i % 3 ==0 and i % 5 == 0):
+        elif (i % 3 == 0 and i % 5 == 0):
             i = FizzBuzz
     print("{}".format(i), end=" ")
