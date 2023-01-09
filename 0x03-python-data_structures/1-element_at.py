ccff1bbaa1f7329a236d8e2b5d8a5023c024e767
malik badmus <malik.badmus@icloud.com>
2023-01-08 23:12:50 -0800

    python list function

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+
+    if idx < 0:
+
+        return None
+
+   elif idx > len(my_list):
+
+        return None
+    else:
+
+        print("Element at {:d} is {}".format(idx, my_list[idx]))
