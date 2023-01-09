cb94606f69b51b411863d6db4cc7ad94617ebfa2
Malik Badmus <46167222+Malikbadmus@users.noreply.github.com>
2023-01-09 08:24:18 +0100

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,11 +4,11 @@ def element_at(my_list, idx):
 
     if idx < 0:
 
-        return None
+        return 'None'
 
    elif idx > len(my_list) - 1:
 
-        return None
+        return 'None'
     else:
 
         return my_list[idx]
