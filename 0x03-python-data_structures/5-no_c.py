18563cfa6767eeb42921321977267a02e9f82e1b
Malik Badmus <46167222+Malikbadmus@users.noreply.github.com>
2023-01-10 02:52:12 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@
 def no_c(my_string):
     new_str = ""
     for x in my_string:
-        if x == 'c' or x == 'C':
+        if x == 'c' and x == 'C':
             continue
         new_str = "".join(x)
         print(new_str, end="")
