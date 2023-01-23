b070338b5a217eeb0e9e25fe0da3b9cdd1d322c4
malik badmus <malik.badmus@icloud.com>
2023-01-23 13:56:53 -0800

    first commit code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,6 +4,6 @@ def safe_print_list(my_list=[], x=0):
         p_list = my_list[:x]
         try:
             print(*p_list, sep = "")
-            print("nb_print: {:d}".format(x))
+            
         except IndexError:
             print("Error in your program")
