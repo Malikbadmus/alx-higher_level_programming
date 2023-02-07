e2070936d41d94c83d74e19eb9a46cbb6ee1e0e2
Malik Badmus <46167222+Malikbadmus@users.noreply.github.com>
2023-02-07 23:41:21 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    with open('filename', encoding = 'utf8') as one:
+    with open(filename, encoding = 'utf8') as one:
         contents = one.read()
         print(contents)
