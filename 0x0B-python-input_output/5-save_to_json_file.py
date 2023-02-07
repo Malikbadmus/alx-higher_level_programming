105a3c0a8b67995b6d88c7202702a4c9e66be6d7
Malik Badmus <46167222+Malikbadmus@users.noreply.github.com>
2023-02-08 00:45:44 +0100

    work work work

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """Write Obj To a File Using JSON."""
 
-
 import json
 
 
 def save_to_json_file(my_obj, filename):
     """Start work."""
-
     newobj = json.dumps(my_obj)
     with open(filename, "w") as f:
         f.write(newobj)
