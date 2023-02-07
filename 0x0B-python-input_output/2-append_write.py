b30fa9a7dcc22050ed7ccc9b5334b0af1564856d
Malik Badmus <46167222+Malikbadmus@users.noreply.github.com>
2023-02-08 00:20:49 +0100

    easy

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,7 +5,7 @@
 def append_write(filename="", text=""):
     """Append a string to a UTF8 file"""
     with open(filename, "a", encoding="utf-8") as f:
-        f.write('\n'.join(text))
+        f.write(text)
         y = 0
         for x in text:
             y += 1
