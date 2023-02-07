9b53f2ae72124e5baeb4b2d35f2d2eba2cb0d73f
malik badmus <malik.badmus@icloud.com>
2023-02-07 15:43:49 -0800

    very good

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write Obj To a File Using JSON."""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Start work."""
+
+    newobj = json.dumps(my_obj)
+    with open(filename, "w") as f:
+        f.write(newobj)
