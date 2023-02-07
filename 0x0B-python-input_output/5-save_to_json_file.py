ba4a3598cebf89aad6fb6126ba5437e05c6fd9c0
Malik Badmus <46167222+Malikbadmus@users.noreply.github.com>
2023-02-08 00:52:22 +0100

    una don start

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,6 +6,6 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Write an object to a text file using JSON representation."""
-    newobj = json.dumps(my_obj)
     with open(filename, "w") as f:
+        newobj = json.dumps(my_obj)
         f.write(newobj)
